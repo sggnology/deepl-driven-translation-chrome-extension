26dd68b3308d1500973287d9b40e6f263310447c
sggnology <sggnology@gmail.com>
2024-05-04 19:11:27 +0900

    [content] API KEY 가 입력되지 않았을 경우 예외 처리

--- content_script.js
+++ content_script.js
@@ -3,46 +3,56 @@
 // 마우스 이벤트를 감지하고 처리하는 함수
 function handleMouseSelection(event) {
     // 선택된 텍스트를 가져옵니다.
-    const selectedText = window.getSelection().toString();
+    let selectedText = window.getSelection().toString();
 
-    chrome.storage.sync.get('apiKey', function(data) {
+    chrome.storage.sync.get('apiKey', function (data) {
 
-        if(data.apiKey == null || data.apiKey == undefined){
-            selectedText = "API KEY 를 입력하여주세요.";
+        let errorMessage = "";
+
+        if (data.apiKey == null || data.apiKey == undefined || data.apiKey == "") {
+            errorMessage = "API KEY 를 입력하여주세요.";
+        }
+        
+        // selectedText trim
+        const trimmedSelectedText = selectedText.trim();
+
+        if (trimmedSelectedText !== "" && trimmedSelectedText !== null) {
+
+            if(errorMessage == "" || errorMessage != null || errorMessage != undefined){
+                addBalloon(errorMessage);
+            }
+            else{
+                addBalloon(selectedText);
+            }
+            
         }
 
     });
 
-    // selectedText trim
-    const trimmedSelectedText = selectedText.trim();
+}
 
-    if(trimmedSelectedText !== "" && trimmedSelectedText !== null){
+function addBalloon(text) {
+    // 선택된 텍스트에 스타일을 적용하여 말풍선을 생성합니다.
+    const balloon = document.createElement("div");
+    balloon.className = "balloon";
+    balloon.style.position = "absolute";
+    balloon.style.maxWidth = "300px";
+    balloon.style.wordBreak = "break-word";
+    balloon.style.padding = "10px";
+    balloon.style.backgroundColor = "black";
+    balloon.style.color = "white";
+    balloon.style.borderRadius = "10px"
+    balloon.innerText = text;
 
-        console.log(`이거 맞어?: ${selectedText}`)
+    // 선택한 텍스트 위에 말풍선을 추가합니다.
+    const selectionRange = window.getSelection().getRangeAt(0);
+    const selectionRect = selectionRange.getBoundingClientRect();
 
-        // 선택된 텍스트에 스타일을 적용하여 말풍선을 생성합니다.
-        const balloon = document.createElement("div");
-        balloon.className = "balloon";
-        balloon.style.position = "absolute";
-        balloon.style.maxWidth = "300px";
-        balloon.style.wordBreak = "break-word";
-        balloon.style.padding = "10px";
-        balloon.style.backgroundColor = "black";
-        balloon.style.color = "white";
-        balloon.style.borderRadius = "10px"
-        balloon.innerText = selectedText;
+    document.body.appendChild(balloon);
 
-        // 선택한 텍스트 위에 말풍선을 추가합니다.
-        const selectionRange = window.getSelection().getRangeAt(0);
-        const selectionRect = selectionRange.getBoundingClientRect();
-
-        document.body.appendChild(balloon);
-
-        // ballon DOM 의 크기는 DOM 에 포함되고 나서야 크기가 결정된다.
-        balloon.style.top = `${selectionRect.top + window.pageYOffset - balloon.offsetHeight - 10}px`;
-        balloon.style.left = `${selectionRect.left + window.pageXOffset}px`;
-    }
-
+    // ballon DOM 의 크기는 DOM 에 포함되고 나서야 크기가 결정된다.
+    balloon.style.top = `${selectionRect.top + window.pageYOffset - balloon.offsetHeight - 10}px`;
+    balloon.style.left = `${selectionRect.left + window.pageXOffset}px`;
 }
 
 // 커서가 해제되면 말풍선을 삭제하는 함수
