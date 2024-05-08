3388223a08fecd30dbff22ddfd408ff212c25746
sggnology <sggnology@gmail.com>
2024-05-08 14:58:49 +0900

    [popup] 이벤트 및 활성화 결정 로직 구현

--- popup.js
+++ popup.js
@@ -1,12 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const setApiKeyButton = document.querySelector("#setApiKeyButton");
-    const apiKeyInput = document.querySelector("input[name=apiKey]");
+// activeState 값이 로드될 때 초기화 및 이벤트 등록
+chrome.storage.sync.get("activeState", (data) => {
 
-    setApiKeyButton.addEventListener("click", () => {
-        // storage 에 API KEY 정보 입력
-        chrome.storage.sync.set({ apiKey: apiKeyInput.value });
+    // [최초] activeState 가 누락되었을 경우 초기화
+    if(data.activeState == null || data.activeState==undefined || data.activeState == ""){
+        chrome.storage.sync.set({ "activeState": "inactive" });
+    }
 
-        // input 에 API KEY 가 표시되면 보안상 취약함으로 빈값
-        apiKeyInput.value = "";
+    console.log("[POPUP] ON/OFF 초기화");
+
+    // [DOM 제어] activeState 값에 따라 ON/OFF 라디오 버튼 체크
+    if (data.activeState === "active") {
+        document.querySelector("input[name=activeState][value=active]").checked = true;
+    } else {
+        document.querySelector("input[name=activeState][value=inactive]").checked = true;
+    }
+
+    console.log(`[POPUP] ON/OFF 현재 값: ${data}`);
+
+    document.addEventListener("DOMContentLoaded", () => {
+
+        const $setActiveStateButton = document.querySelector("#setActiveStateButton");
+    
+        // [DOM 이벤트] ON/OFF 라디오 버튼 클릭 시 activeState 값 변경
+        $setActiveStateButton.addEventListener("click", () => {
+            const activeState = document.querySelector("input[name=activeState]:checked").value;
+            chrome.storage.sync.set({ "activeState": activeState });
+    
+            console.log(`[POPUP] ON/OFF 를 ${activeState} 로 변경하였습니다.`);
+        });
     });
 });
