d28677a247cfece9b0d18ddcf118e9c02b5bf918
sggnology <sggnology@gmail.com>
2024-05-08 14:09:51 +0900

    [content] deepl 은 제한이 있음으로 reservo 로 변경

--- content_script.js
+++ content_script.js
@@ -5,46 +5,14 @@ function handleMouseSelection(event) {
     // 선택된 텍스트를 가져옵니다.
     let selectedText = window.getSelection().toString();
 
-    const url = `https://www2.deepl.com/jsonrpc?method=LMT_handle_jobs`;
-    const body = generateBody({ sourceText: selectedText });
-
+    const url = `https://translation-proxy.sggnology.workers.dev`;
+    
     const trimmedSelectedText = selectedText.trim();
 
     if (trimmedSelectedText !== "" && trimmedSelectedText !== null) {
 
-        // const xhr = new XMLHttpRequest();
-        // xhr.open('POST', url, true);
-        // xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-        // xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-        // xhr.onreadystatechange = function() {
-        //     if (xhr.readyState === 4 && xhr.status === 200) {
-        //         const data = JSON.parse(xhr.responseText);
-        //         // 번역된 결과값들
-        //         const translatedCandidates = data?.result?.translations?.beams;
-        //         let resultText = "";
+        const body = {"source": trimmedSelectedText};
 
-        //         // 번역 응답 API 에 문제가 발생했을 경우
-        //         if(translatedCandidates == null){
-        //             resultText = "번역에 실패하였습니다.";
-        //         }
-        //         // 번역된 결과가 없을 경우
-        //         else if(translatedCandidates.length == 0){
-        //             resultText = "번역된 결과가 없습니다.";
-        //         }
-        //         // 번역이 되었을 경우
-        //         else{
-
-        //             // 첫번째 값을 번역된 결과로 서빙
-        //             resultText = translatedCandidates[0]?.sentences[0]?.text;
-        //         }
-
-        //         addBalloon(selectedText);
-        //     } else if (xhr.readyState === 4) {
-        //         // 번역 API 요청 오류 발생시
-        //         addBalloon("번역 요청 과정에서 오류가 발생하였습니다.");
-        //     }
-        // };
-        // xhr.send(JSON.stringify(body));
         fetch(url, {
             mode: 'cors',
             method: 'POST',
@@ -57,25 +25,26 @@ function handleMouseSelection(event) {
         .then(data => {
 
             // 번역된 결과값들
-            const translatedCandidates = data?.result?.translations?.beams;
-            let resultText = "";
+            const translatedCandidates = data?.translation;
+
+            let result = "";
 
             // 번역 응답 API 에 문제가 발생했을 경우
             if(translatedCandidates == null){
-                resultText = "번역에 실패하였습니다.";
+                result = "번역에 실패하였습니다.";
             }
             // 번역된 결과가 없을 경우
             else if(translatedCandidates.length == 0){
-                resultText = "번역된 결과가 없습니다.";
+                result = "번역된 결과가 없습니다.";
             }
             // 번역이 되었을 경우
             else{
 
                 // 첫번째 값을 번역된 결과로 서빙
-                resultText = translatedCandidates[0]?.sentences[0]?.text;
+                result = translatedCandidates[0];
             }
 
-            addBalloon(selectedText);
+            addBalloon(result);
         })
         .catch(error => {
             // 번역 API 요청 오류 발생시
@@ -123,79 +92,3 @@ document.addEventListener("mouseup", removeBalloon);
 
 // 마우스 이벤트 핸들러를 등록합니다.
 document.addEventListener("mouseup", handleMouseSelection);
-
-function requestForTranslation({ sourceText }) {
-    
-}
-
-function generateBody({ sourceText }) {
-    return {
-        "jsonrpc": "2.0",
-        "method": "LMT_handle_jobs",
-        "params": {
-            "jobs": [
-                {
-                    "kind": "default",
-                    "sentences": [
-                        {
-                            "text": sourceText,
-                            "id": 1,
-                            "prefix": ""
-                        }
-                    ],
-                    "raw_en_context_before": [],
-                    "raw_en_context_after": [],
-                    "preferred_num_beams": 4,
-                    "quality": "fast"
-                }
-            ],
-            "lang": {
-                "target_lang": "KO",
-                "preference": {
-                    "weight": {
-                        "DE": 0.13462,
-                        "EN": 11.50693,
-                        "ES": 0.07732,
-                        "FR": 0.1047,
-                        "IT": 0.03269,
-                        "JA": 0.04014,
-                        "NL": 0.03271,
-                        "PL": 0.02464,
-                        "PT": 0.02413,
-                        "RU": 0.02711,
-                        "ZH": 0.03099,
-                        "BG": 0.0128,
-                        "CS": 0.0132,
-                        "DA": 0.01255,
-                        "EL": 0.01212,
-                        "ET": 0.01116,
-                        "FI": 0.01379,
-                        "HU": 0.0124,
-                        "ID": 0.0122,
-                        "KO": 0.01238,
-                        "LV": 0.0098,
-                        "LT": 0.01144,
-                        "NB": 0.01379,
-                        "RO": 0.01209,
-                        "SK": 0.01202,
-                        "SL": 0.01172,
-                        "SV": 0.01458,
-                        "TR": 0.01254,
-                        "UK": 0.01469,
-                        "AR": 0.01333
-                    },
-                    "default": "default"
-                },
-                "source_lang_user_selected": "EN"
-            },
-            "priority": -1,
-            "commonJobParams": {
-                "mode": "translate",
-                "browserType": 1,
-                "textType": "plaintext"
-            },
-            "timestamp": new Date().getTime()
-        },
-        "id": 37110010
-    }
-}
